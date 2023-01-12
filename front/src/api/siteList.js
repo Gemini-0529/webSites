b230d0c23ebe07c8e6164b997d61aae4b0fd6621
zhiwenbin <72254385+Gemini-0529@users.noreply.github.com>
2023-01-12 17:02:19 +0800

    Update siteList.js

--- front/src/api/siteList.js
+++ front/src/api/siteList.js
@@ -1,61 +1,66 @@
 import axios from "@/axios.js";
-function getSiteList(params) {
+export function getSiteList(params) {
   return axios.get("/API/site/siteList", {
     params,
   });
 }
-function addSite(params) {
+export function addSite(params) {
   return axios.post("/API/site/addSite", {
     ...params,
   });
 }
-function delSite(params) {
+export function delSite(params) {
   return axios.post("/API/site/delSite", {
     ...params,
   });
 }
-function colSite(params) {
+export function colSite(params) {
   return axios.post("/API/site/colSite", {
     ...params,
   });
 }
 
 // 爬取网站icon
-function spliderIcon(params) {
+export function spliderIcon(params) {
   return axios.get("/API/site/getIco", {
     params,
   });
 }
 // 获取网站详情
-function siteDetail(params) {
+export function siteDetail(params) {
   return axios.get("/API/site/siteDetail", {
     params,
   });
 }
 // 修改网站
-function updateSite(params) {
+export function updateSite(params) {
   return axios.post("/API/site/updateSite", {
     ...params,
   });
 }
 // 经常访问
-function frequentlyVisited() {
+export function frequentlyVisited() {
   return axios.get("API/site/frequentlyVisited",{})
 }
 // 点击后增加访问次数
-function addVisitTimes(params) {
+export function addVisitTimes(params) {
   return axios.post("/API/site/addVisitTimes", {
     ...params
   })
 }
-export {
-  getSiteList,
-  addSite,
-  delSite,
-  spliderIcon,
-  colSite,
-  siteDetail,
-  updateSite,
-  frequentlyVisited,
-  addVisitTimes,
-};
+export function visitedHistory(params) {
+  return axios.get('/API/site/history', {
+    params
+  })
+}
+// export {
+//   getSiteList,
+//   addSite,
+//   delSite,
+//   spliderIcon,
+//   colSite,
+//   siteDetail,
+//   updateSite,
+//   frequentlyVisited,
+//   addVisitTimes,
+// };
