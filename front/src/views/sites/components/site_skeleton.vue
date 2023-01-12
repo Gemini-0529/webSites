1a931a96e9e6cbb1c16c583165b9b0d669564ec0
zhiwenbin <72254385+Gemini-0529@users.noreply.github.com>
2023-01-12 15:32:33 +0800

    Create site_skeleton.vue

---
+++ front/src/views/sites/components/site_skeleton.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { defineProps, ref } from "vue";
+const props = defineProps({
+  loading: Boolean,
+});
+</script>
+
+<template>
+  <el-skeleton id="skeleton" animated :throttle="500" :loading="props.loading">
+    <template #template>
+      <el-skeleton-item variant="circle" style="width: 100px; height: 100px" />
+      <div class="item">
+        <el-skeleton-item />
+        <el-skeleton-item />
+        <el-skeleton-item />
+      </div>
+    </template>
+  </el-skeleton>
+</template>
+<style scoped lang="scss">
+#skeleton {
+  width: 310px;
+  height: 160px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  .item {
+    flex: 1;
+    margin-left: 10px;
+    .el-skeleton__item:last-child {
+      width: 40%;
+    }
+  }
+}
+</style>
