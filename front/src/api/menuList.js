2da3474c8a24cb055fa3a49f12f34b382431cd75
zhiwenbin <72254385+Gemini-0529@users.noreply.github.com>
2023-04-12 17:25:06 +0800

    Update menuList.js

--- front/src/api/menuList.js
+++ front/src/api/menuList.js
@@ -1,16 +1,15 @@
 import axios from '@/axios.js'
 
-function menuList(params) {
+export function menuList(params) {
   return axios.get('/API/menu/menuList', {
     params
   })
+}
+export function addLeftMenu(params) {
+  return axios.post('/API/menu/addMenu', params)
 }
 // function testpost(params) {
 //   return axios.post('/API/xxx', {
 //     ...params
 //   })
 // }
-
-export {
-  menuList
-}
