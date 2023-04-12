053a036be586c256c3a77c54ffe0181eef9c6346
zhiwenbin <72254385+Gemini-0529@users.noreply.github.com>
2023-04-12 17:24:11 +0800

    Update menu_router.js

--- node/router/menu_router.js
+++ node/router/menu_router.js
@@ -6,5 +6,13 @@ router.get('/menuList', (req, response)=> {
   let sql = 'select * from menuList'
   getDataFromDb(sql, null, response)
 })
-
+router.post('/addMenu', (req, response) => {
+  const {parentId, label, checkAuthority=1, level} = req.body
+  const sql = `insert into menulist
+    (parentId, label, checkAuthority, level)
+    values
+    (${parentId}, '${label}', '${checkAuthority}', '${level}')
+  `;
+  editDbData(sql, response)
+})
 module.exports = router
