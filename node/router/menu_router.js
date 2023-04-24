558cbe73e61ee2462c62f70a0238c41f73abcfc9
zhiwenbin <72254385+Gemini-0529@users.noreply.github.com>
2023-04-24 15:23:08 +0800

    关联用户uid

--- node/router/menu_router.js
+++ node/router/menu_router.js
@@ -3,15 +3,15 @@ const router = express.Router()
 const { getDataFromDb } = require('../utils/index.js')
 
 router.get('/menuList', (req, response)=> {
-  let sql = 'select * from menuList'
+  let sql = `select * from menuList where uid=${req.query.uid}`
   getDataFromDb(sql, response, false)
 })
 router.post('/addMenu', (req, response) => {
-  const {parentId, label, checkAuthority=1, level} = req.body
+  const {parentId, label, checkAuthority=1, level, uid} = req.body
   const sql = `insert into menulist
-    (parentId, label, checkAuthority, level)
+    (parentId, label, checkAuthority, level, uid)
     values
-    (${parentId}, '${label}', '${checkAuthority}', '${level}')
+    (${parentId}, '${label}', '${checkAuthority}', '${level}', ${uid})
   `;
   editDbData(sql, response)
 })
