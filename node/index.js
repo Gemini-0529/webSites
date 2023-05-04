8edaec0fc1aaab62d678529138391d13e47ae536
zhiwenbin <72254385+Gemini-0529@users.noreply.github.com>
2023-05-04 13:28:11 +0800

    服务器接口预请求

--- node/index.js
+++ node/index.js
@@ -12,12 +12,12 @@ const commonRouter = require('./router/common_router')
 
 
 app.listen(3000, ()=>{
-    console.log('---server start---');
+    console.log('---server start--- 3000端口');
 })
 // 托管静态资源
 app.use('/images',express.static('images'))
 app.all('*', (req, res, next) => {
-    res.header("Content-Type", "application/json;charset=utf-8")
+    res.header("Content-Type", "application/json;charset=utf-8");
     next()
 })
 // 解析post请求参数
@@ -30,11 +30,15 @@ app.use('/register', registerRouter)
 
 // 校验token中间件
 app.use((req,res,next) => {
+    if(req.method == 'OPTIONS') {
+        next()
+    }
     const token = req.headers.token
     const validToken = jwt.decrypt(token)
-    // 验证失败返回401
+    // // 验证失败返回401
     validToken ? next() : res.status(401).send('unAuthority')
 })
+
 // 菜单相关路由
 app.use('/menu', menuRouter)
 // 网站相关路由
