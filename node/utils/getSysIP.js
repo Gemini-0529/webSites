885a9cbedac297fe7a5aca7f1f3dc05ae9af5cde
zhiwenbin <72254385+Gemini-0529@users.noreply.github.com>
2023-05-05 11:02:56 +0800

    Create getSysIP.js

---
+++ node/utils/getSysIP.js
@@ -0,0 +1,14 @@
+const os = require('os')
+// 获取当前系统 IPaddress
+module.exports = function () {
+  const interfaces = os.networkInterfaces()
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+        return alias.address;
+      }
+    }
+  }
+}
