4f22cbfe514d11adac790986194f5d1ee5aa74ab
zhiwenbin <72254385+Gemini-0529@users.noreply.github.com>
2023-05-06 11:04:57 +0800

    add 查询父级菜单接口

--- node/router/menu_router.js
+++ node/router/menu_router.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const { getDataFromDb, editDbData } = require('../utils/index.js')
-
-router.get('/menuList', (req, response)=> {
-  let sql = `select * from menuList where uid=${req.query.uid}`
-  getDataFromDb(sql, response, false)
+// 获取菜单列表
+router.get('/menuList', (req, response) => {
+  const sql = `select * from menuList where uid=${req.query.uid}`
+  getDataFromDb(sql, response)
 })
+
+// 添加二级菜单时获取所有父菜单
+router.get('/parentMenu', (req, response) => {
+  const sql = `select * from menuList where uid=${req.query.uid} and level=1`
+  getDataFromDb(sql, response)
+})
+
+// 添加菜单
 router.post('/addMenu', (req, response) => {
   const {parentId, label, checkAuthority=1, level, uid} = req.body
   const sql = `insert into menulist
@@ -15,4 +23,5 @@ router.post('/addMenu', (req, response) => {
   `;
   editDbData(sql, response)
 })
+
 module.exports = router
