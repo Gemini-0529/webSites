372b19b3027204989bc5c12dd7cefa21bd29c7ce
zhiwenbin <72254385+Gemini-0529@users.noreply.github.com>
2023-05-06 11:09:37 +0800

    Update addMenuDialog.vue

--- front/src/views/aside/addMenuDialog.vue
+++ front/src/views/aside/addMenuDialog.vue
@@ -1,6 +1,8 @@
 <script setup>
-import { reactive, defineProps, defineEmits, ref } from "vue";
-import { addLeftMenu } from "@/api/menuList.js";
+import { reactive, defineProps, defineEmits, ref, watch } from "vue";
+import { addLeftMenu, parentMenu } from "@/api/menuList.js";
+import { ElMessage } from "element-plus";
+import { useMenu } from "@/stores/menuList.js";
 const props = defineProps({
   showDialog: {
     default: false,
@@ -8,23 +10,50 @@ const props = defineProps({
     required: true,
   },
 });
+const uid = localStorage.getItem("uid") * 1;
+// 获取一级菜单
+const parentMenus = reactive({
+  data: [],
+});
+async function getParentMenu() {
+  const res = await parentMenu({ uid });
+  if (res.status === 200 && res.data.length) {
+    parentMenus.data = res.data;
+  } else {
+    ElMessage.error("获取父级菜单列表失败");
+  }
+}
+
 let formData = reactive({
   data: {
     label: "",
     parentId: "",
     level: 1,
-    uid: localStorage.getItem('uid') * 1
+    uid,
   },
 });
-
+watch(
+  () => formData.data.level,
+  (nval, oval) => {
+    // 选择二级菜单，获取当前用户创建的一级菜单
+    if (nval == 2) {
+      getParentMenu();
+    }
+  }
+);
 const emits = defineEmits(["update:showDialog"]);
+const menuStore = useMenu();
 async function submit() {
   const params = formData.data;
   if (params.level == 1) {
     params.parentId = null;
   }
   const res = await addLeftMenu(params);
-  emits('update:showDialog', false)
+  if (res.status === 200) {
+    ElMessage.success(`新增 ${params.label} 菜单成功`);
+    await menuStore.getMenuList();
+  }
+  emits("update:showDialog", false);
 }
 const form = ref(null);
 function close() {
@@ -42,23 +71,27 @@ function close() {
     v-if="showDialog"
   >
     <el-form :model="formData" ref="form" label-position="right">
-      <el-form-item label="名称" label-width="80px">
+      <el-form-item label="名称：" label-width="100px">
         <el-input v-model="formData.data.label" autocomplete="off" />
       </el-form-item>
-      <el-form-item label="菜单级别" label-width="80px">
+      <el-form-item label="菜单级别：" label-width="100px">
         <el-radio-group v-model="formData.data.level">
           <el-radio :label="1">一级</el-radio>
           <el-radio :label="2">二级</el-radio>
         </el-radio-group>
       </el-form-item>
       <el-form-item
-        label="父菜单"
+        label="父级菜单："
         v-if="formData.data.level == 2"
-        label-width="80px"
+        label-width="100px"
       >
-        <el-select v-model="formData.data.parentId" placeholder="Please">
-          <el-option label="Zone No.1" value="shanghai" />
-          <el-option label="Zone No.2" value="beijing" />
+        <el-select v-model="formData.data.parentId" placeholder="请选择父级菜单">
+          <el-option
+            :label="item.label"
+            :value="item.id"
+            :key="item.id"
+            v-for="item in parentMenus.data"
+          />
         </el-select>
       </el-form-item>
     </el-form>
