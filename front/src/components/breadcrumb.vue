1c06b9aa5af1a9541f685e400b5b218e3c3628ad
zhiwenbin <72254385+Gemini-0529@users.noreply.github.com>
2023-05-31 13:48:01 +0800

    Update breadcrumb.vue

--- front/src/components/breadcrumb.vue
+++ front/src/components/breadcrumb.vue
@@ -1,19 +1,16 @@
 <script setup>
-import { useRouter } from 'vue-router'
-import { useMenu } from "@/stores/menuList.js";
-import { onMounted } from 'vue';
-const menuStore = useMenu()
-const router = useRouter()
-const curMenu = menuStore.curMenuData?.value
-onMounted(()=>{
-  console.log(curMenu);
-})
+import { useRouter } from "vue-router";
+import { computed } from "vue";
+const router = useRouter();
+const breadItem = computed(() => {
+  return router.currentRoute.value.meta.title;
+});
 </script>
 <template>
-<div id="breadCrumb">
-  <el-breadcrumb separator-icon="ArrowRight">
-    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-    <el-breadcrumb-item>网站列表</el-breadcrumb-item>
-  </el-breadcrumb>
-</div>
+  <div id="breadCrumb">
+    <el-breadcrumb separator-icon="ArrowRight">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>{{ breadItem }}</el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
 </template>
